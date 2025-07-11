144dc01bbbc5577513a98b80822744ef81fb1395
Rocco Sicilia <rocco.sicilia@protonmail.com>
2025-07-11 12:14:02 +0200

    test hostname

---
+++ scripts/export_bloodhound_info.py
@@ -0,0 +1,14 @@
+import json
+import sys
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <input_file.json>")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+
+with open(input_file, "r") as f:
+    data = json.load(f)
+
+for element in data.get("Aces", []):
+    print(f"Hostname: {element['name']}, RightName: {element['RightName']}")
