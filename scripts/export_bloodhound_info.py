a146d3b3812a39c7a1b27e19fb0630291a0e7b9f
Rocco Sicilia <rocco.sicilia@protonmail.com>
2025-07-11 15:45:15 +0200

    update condition

--- scripts/export_bloodhound_info.py
+++ scripts/export_bloodhound_info.py
@@ -2,18 +2,21 @@ import json
 import sys
 import os
 
-if len(sys.argv) != 2:
-    print(f"Usage: {sys.argv[0]} <input_file.json>")
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} <input_file.json> <full|null>")
     sys.exit(1)
 
 input_file = sys.argv[1]
+full = sys.argv[2]
 
 with open(input_file, "r", encoding="utf-8-sig") as f:
     data = json.load(f)
 
-# print a list of interesting Windows hosts
-hostlist = []
+oldhostlist = []
+srvhostlist = []
 fulllist = []
+
+# print a list of interesting Windows hosts
 print("\n##### Interesting Windows hosts:")
 for entry in data.get("data", []):
     properties = entry.get("Properties", {})
@@ -22,21 +25,34 @@ for entry in data.get("data", []):
     fulllist.append(hostname)
     if isinstance(operatingsystem, str) and ("2003" in operatingsystem or "2008" in operatingsystem or "XP" in operatingsystem or "Windows 7" in operatingsystem):
         print(f"Hostname: {hostname}, operatingsystem: {operatingsystem}")
-        hostlist.append(hostname)
+        oldhostlist.append(hostname)
 
 # check if the interesting host is online
 print("\n##### Checking if hosts are online:")
-for host in hostlist:
+for host in oldhostlist:
     response = os.system(f"ping -c 1 {host} > /dev/null 2>&1")
     if response == 0:
         print(f"{host} is online with IP address {os.popen(f'getent hosts {host}').read().strip().split()[0]}")
     else:
         print(f"{host} is offline")
 
-# check for any online hosts
-print("\n##### Checking for any online hosts:")
-for host in fulllist:
-    response = os.system(f"ping -c 1 {host} > /dev/null 2>&1")
-    if response == 0:
-        print(f"{host} is online with IP address {os.popen(f'getent hosts {host}').read().strip().split()[0]}")
+# print a list of Windows hosts with SMB shares
+print("\n##### Windows hosts:")
+for entry in data.get("data", []):
+    properties = entry.get("Properties", {})
+    hostname = properties.get("name", "N/A")
+    operatingsystem = properties.get("operatingsystem", "N/A")
+    fulllist.append(hostname)
+    if isinstance(operatingsystem, str) and ("Server" in operatingsystem):
+        print(f"Hostname: {hostname}, operatingsystem: {operatingsystem}")
+        oldhostlist.append(hostname)
 
+# check for any online hosts
+if full == 'full':
+    print("\n##### Checking for any online hosts:")
+    for host in fulllist:
+        response = os.system(f"ping -c 1 {host} > /dev/null 2>&1")
+        if response == 0:
+            print(f"{host} is online with IP address {os.popen(f'getent hosts {host}').read().strip().split()[0]}")
+else:
+    print("\n##### Skipping online check for hosts as 'full' is not specified.")
